d7f41618aa6b184a5179b76c24e9116b7d2db2f7
madhav <maddy.kovelamudi@gmail.com>
2019-12-10 14:54:29 -0500

    sean is baaaaad

--- uat/Tables/dbo.Table_1.sql
+++ uat/Tables/dbo.Table_1.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Table_1]
 (
 [name] [nchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[id] [integer] COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[id] [int] NULL,
+[cal1] [ntext] COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
