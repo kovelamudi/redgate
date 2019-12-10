81c8482e03a74c48f0454bae0881b2d83e30fcfe
kovelamudi <48769677+kovelamudi@users.noreply.github.com>
2019-12-10 14:45:59 -0500

    test
    test

--- uat/Tables/dbo.Table_1.sql
+++ uat/Tables/dbo.Table_1.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[Table_1]
 (
-[name] [nchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[name] [nchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [id] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
