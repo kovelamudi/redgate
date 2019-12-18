1753dcd0809a7f043856f49be4bc2184ec41c337
madhav <maddy.kovelamudi@gmail.com>
2019-12-18 09:36:01 -0500

    initial setup 2019

---
+++ VersionSQL/DESKTOP-FVA5MG7/uat1/dbo/Procedure/sp_Test.sql
@@ -0,0 +1,12 @@
+/****** Object:  Procedure [dbo].[sp_Test]    Committed by VersionSQL https://www.versionsql.com ******/
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[sp_Test]
+	@V AS INT = 0
+AS
+
+SELECT SYSDATETIME()
